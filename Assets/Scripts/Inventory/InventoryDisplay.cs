689cc9504078a0b79969d527303306f9cfc495d8
USER_1958 <wolf101x7@gmail.com>
2023-04-29 16:39:15 -0700

    Inventory hotbar and items

---
+++ Assets/Scripts/Inventory/InventoryDisplay.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+public abstract class InventoryDisplay : MonoBehaviour
+{
+    [SerializeField] MouseItemData mouseInventoryItem;
+
+    protected InventorySystem inventorySystem;
+    protected Dictionary<InventorySlot_UI, InventorySlot> slotDictionary;
+    public InventorySystem InventorySystem => inventorySystem;
+    public Dictionary<InventorySlot_UI, InventorySlot> SlotDictionary => slotDictionary;
+
+    protected virtual void Start()
+    {
+
+    }
+
+    public abstract void AssignSlot(InventorySystem invToDisplay);
+
+    protected virtual void UpdateSlot(InventorySlot updatedSlot)
+    {
+        foreach (var slot in SlotDictionary)
+        {
+            if (slot.Value == updatedSlot) // Slot Value - "Under the hood" inventory slot value
+            {
+                slot.Key.UpdateUISlot(updatedSlot); // Slot key - the UI representation of the value
+            }
+        }
+    }
+
+    public void SlotClicked(InventorySlot_UI clickedSlot)
+    {
+        Debug.Log("Slot clicked");
+    }
+}
